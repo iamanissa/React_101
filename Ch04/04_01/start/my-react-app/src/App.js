19dbaa3a24769f277903aec5c7d605e624598f41
Anissa <anissajones10@gmail.com>
2024-03-12 22:12:07 -0400

    Try out useEffect method

--- Ch04/04_01/start/my-react-app/src/App.js
+++ Ch04/04_01/start/my-react-app/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(()=>{
+    console.log(`It's ${emotion} right now`)
+  }, [emotion, secondary]);
+  useEffect(()=>{
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary]);
 
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={()=>setEmotion("sad")}>Sad</button>
-      <button onClick={()=>setEmotion("excited")}>Excited</button>
+      <button onClick={()=>setEmotion("excited")}>Excited</button>     
+      <h1>Current secondary is {secondary}</h1>
+      <button onClick={()=>setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }
