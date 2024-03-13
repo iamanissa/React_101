0b1a16ed3582c416c671e2df1895244e9d59c15b
Anissa <anissajones10@gmail.com>
2024-03-12 22:24:32 -0400

    Use useReducer function and complete part 4

--- Ch04/04_01/start/my-react-app/src/App.js
+++ Ch04/04_01/start/my-react-app/src/App.js
@@ -1,24 +1,19 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(()=>{
-    console.log(`It's ${emotion} right now`)
-  }, [emotion, secondary]);
-  useEffect(()=>{
-    console.log(`It's ${secondary} around here!`)
-  }, [secondary]);
-
+  const [checked, setChecked] = useReducer(
+    (checked) => !checked,
+    false
+  );
   return (
     <div className="App">
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={()=>setEmotion("sad")}>Sad</button>
-      <button onClick={()=>setEmotion("excited")}>Excited</button>     
-      <h1>Current secondary is {secondary}</h1>
-      <button onClick={()=>setSecondary("grateful")}>Grateful</button>
+      <input 
+        type="checkbox" 
+        value={checked} 
+        onChange={setChecked}
+      />
+      <label>{checked ? "Checked" : "Not Checked"}</label>
     </div>
   );
 }
