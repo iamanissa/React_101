0110a09993f6895827f6429faada8debe6141f64
Anissa <anissajones10@gmail.com>
2024-03-15 16:06:50 -0400

    Finish chapter 7: React Router

--- Ch07/07_01_and_02/start/react-app/src/App.js
+++ Ch07/07_01_and_02/start/react-app/src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
 
-function Home() {
+function Home(){
   return (
     <div>
-      <h1>My Website</h1>
+      <h1>My website</h1>
     </div>
-  );
+  )
 }
 
-function About() {
+export function About(){
   return (
     <div>
       <h1>About Us</h1>
     </div>
-  );
+  )
 }
 
-function Contact() {
+export function Contact(){
   return (
     <div>
       <h1>Contact Us</h1>
     </div>
+  )
+}
+
+export function App() {
+  return (
+    <Home />
   );
 }
-
-function App() {
-  return <Home />;
-}
-
-export default App;
